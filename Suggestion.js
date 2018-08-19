cfb65d50d9f5b3bbe0ad356b8399912bc1d96378
Ryan <lucky.tanduzhi@gmail.com>
2018-08-20 04:56:41 +0800

    ✨ 完成 Suggestion 组件

---
+++ Suggestion.js
@@ -0,0 +1,78 @@
+class Suggestion {
+  constructor(options) {
+    this.options = options
+
+    this.$input = $(this.options.input)
+
+    this.$input.wrap('<div class="ZakasSuggestion"></div>')
+    this.$wrapper = this.$input.parent()
+
+    this.$ol = $('<ol class="ZakasSuggestion-list"></ol>')
+    this.$input.after(this.$ol)
+
+    this.$loading = $('<div class="ZakasSuggestion-loading"></div>')
+    this.$loading.html(this.options.loading)
+    this.$input.after(this.$loading)
+
+    this.$empty = $('<div class="ZakasSuggestion-empty"></div>')
+    this.$empty.html(this.options.emptyHint)
+    this.$input.after(this.$empty)
+
+    this.eventBind()
+  }
+  eventBind() {
+    var timerID
+    this.$input.on('input', (e) => {
+      if (timerID) {
+        window.clearTimeout(timerID)
+      }
+      timerID = setTimeout(() => {
+        this.search(e.target.value)
+      }, 200)
+    })
+
+    this.$ol.on('click','li',(e)=>{
+      this.$input.val(e.target.innerText)    
+    })
+  }
+
+
+  search(keyword) {
+    console.log('开始搜索')
+    this.$wrapper.addClass('loading')
+    this.options.search(keyword, (array) => {
+      
+      this.$wrapper.removeClass('loading empty')
+      this.$ol.empty()
+
+      if (!array || array.length === 0){
+        this.$wrapper.addClass('empty')
+        console.log('kong')
+      }
+
+      array.forEach(text => {
+        var $li = $('<li></li>')
+        $li.text(text)
+        this.$ol.append($li)
+      })
+    })
+  }
+}
+
+var s = new Suggestion({
+  input: '.input',
+  search: function (text, callback) {
+    if (text === '0'){
+      setTimeout(()=>{callback([])}, 300)
+      return
+    }
+    arr = []
+    for (var i = 0; i < 5; i++) {
+      var n = parseInt(Math.random() * 100, 10)
+      arr.push(text + n)
+    }
+    setTimeout(() => { callback(arr) }, 300)
+  },
+  loading: '正在加载',
+  emptyHint: '没找到结果'
+})
