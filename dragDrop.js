fb070d2ade1ba2b93e538ac188b3ae6f49b4a796
Ryan <lucky.tanduzhi@gmail.com>
2018-08-22 14:30:55 +0800

    ♻️ 封装拖拽对象

--- dragDrop.js
+++ dragDrop.js
@@ -1,82 +1,100 @@
-function getTransform() {
-  var transform = '',
-    divstyle = document.createElement('div').style,
-    transformArr = ['transform', 'webkitTransform', 'MozTransform', 'msTransform', 'OTransform']
-  i = 0
-  len = transformArr.length
+;
+(function () {
+  function Drag(selecotr) {
+    this.elem = typeof selecotr === 'Object' ? selecotr : document.querySelector(selecotr)
+    this.startX = 0
+    this.startY = 0
+    this.sourceX = 0
+    this.sourceY = 0
 
-  for (; i < len; i++) {
-    if (transformArr[i] in divstyle) {
-      return transform = transformArr[i]
-    } else {
-      return transform
-    }
+    this.init()
   }
-}
 
-function getStyle(elem, property) {
-  return document.defaultView.getComputedStyle(elem, false)[property]
-}
+  Drag.prototype = {
+    constructor: Drag,
+    init: function () {
+      this.setDrag()
+    },
+    getStyle: function (property) {
+      return document.defaultView.getComputedStyle(this.elem, false)[property]
+    },
 
-function getTargetPos(elem) {
-  var pos = { x: 0, y: 0 }
-  var transform = getTransform()
-  if (transform) {
-    var transfromValue = getStyle(elem, transform)
-    if (transfromValue == 'none') {
-      elem.style.transform = 'translate(0, 0)'
-      return pos
-    } else {
-      var temp = transfromValue.match(/-?\d+/g)
-      return pos = {
-        x: parseInt(temp[4].trim()),
-        y: parseInt(temp[5].trim())
+    getPosition: function () {
+      var pos = { x: 0, y: 0 }
+      var transform = getTransform()
+      if (transform) {
+        var transfromValue = this.getStyle(transform)
+        if (transfromValue == 'none') {
+          this.elem.style.transform = 'translate(0, 0)'
+          return pos
+        } else {
+          var temp = transfromValue.match(/-?\d+/g)
+          return pos = {
+            x: parseInt(temp[4].trim()),
+            y: parseInt(temp[5].trim())
+          }
+        }
+      }
+    },
+    setPosition: function (pos) {
+      var transform = getTransform()
+      if (transform) {
+        this.elem.style[transform] = 'translate(' + pos.x + 'px, ' + pos.y + 'px)'
+      }
+    },
+
+    setDrag: function () {
+      var self = this
+      this.elem.addEventListener('mousedown', start, false)
+
+      function start(event) {
+        self.startX = event.pageX
+        self.startY = event.pageY
+
+        var pos = self.getPosition()
+
+        self.sourceX = pos.x
+        self.sourceY = pos.y
+
+        document.addEventListener('mousemove', move, false)
+        document.addEventListener('mouseup', end, false)
+      }
+
+      function move(event) {
+        var currentX = event.pageX
+        var currentY = event.pageY
+
+        var distanceX = currentX - self.startX
+        var distanceY = currentY - self.startY
+
+        self.setPosition({
+          x: (distanceX + self.sourceX).toFixed(),
+          y: (distanceY + self.sourceY).toFixed()
+        })
+      }
+
+      function end(event) {
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', end)
       }
     }
   }
-}
 
-function setTargetPos(elem, pos) {
-  var transform = getTransform()
-  if (transform) {
-    elem.style[transform] = 'translate(' + pos.x + 'px, ' + pos.y + 'px)'
+  function getTransform() {
+    var transform = '',
+      divstyle = document.createElement('div').style,
+      transformArr = ['transform', 'webkitTransform', 'MozTransform', 'msTransform', 'OTransform']
+    i = 0
+    len = transformArr.length
+
+    for (; i < len; i++) {
+      if (transformArr[i] in divstyle) {
+        return transform = transformArr[i]
+      } else {
+        return transform
+      }
+    }
   }
-}
 
-var movingElem = document.querySelector('.elem1')
-var startX = 0
-var startY = 0
-var sourceX = 0
-var sourceY = 0
-movingElem.addEventListener('mousedown', start, false)
-
-function start(event) {
-  startX = event.pageX
-  startY = event.pageY
-
-  var pos = getTargetPos(movingElem)
-
-  sourceX = pos.x
-  sourceY = pos.y
-
-  document.addEventListener('mousemove', move, false)
-  document.addEventListener('mouseup', end, false)
-}
-
-function move(event){
-  var currentX = event.pageX
-  var currentY = event.pageY
-
-  var distanceX = currentX - startX
-  var distanceY = currentY - startY
-
-  setTargetPos(movingElem, {
-    x: (distanceX + sourceX).toFixed(),
-    y: (distanceY + sourceY).toFixed()
-  })
-}
-
-function end(event){
-  document.removeEventListener('mousemove', move)
-  document.removeEventListener('mouseup', end)
-}
+  window.Drag = Drag
+})()
